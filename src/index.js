8852463a5411433f4febe2b6eec6c75586626a61
n_meeks <najja.meeks3@gmail.com>
2026-04-22 12:45:06 -0700

    Built data model

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import NameTag from "./NameTag";
+import {names} from "./data.js";
 import "./styles.css";
 
-const App = (props) => (
+const renderNameTag = (name) => <NameTag name={name.person} key={name.id} />;
+
+const App = (props) => {
+  const NameTagElements = names.map(renderNameTag);
+
+  return (
   <div className="App">
     <h1>Name Tag Generator</h1>
-    <NameTag name="Naima"/>
-    <NameTag name="Camila"/>
-    <NameTag name="Anthony"/>
-    <NameTag name="Maceo"/>
-    <NameTag name="Laila"/>
+    {NameTagElements}
+
   </div>
-);
+  );
+};
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
