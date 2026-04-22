66eef7d27027b01d24f73301ff302e2b5038c2e9
n_meeks <najja.meeks3@gmail.com>
2026-04-22 13:52:39 -0700

    Added array

---
+++ src/data.js
@@ -0,0 +1,22 @@
+export const names = [
+  {
+    id: 10,
+    person: "Naima",
+  },
+  {
+    id: 20,
+    person: "Camila",
+  },
+  {
+    id: 30,
+    person: "Anthony",
+  },
+  {
+    id: 40,
+    person: "Maceo",
+  },
+  {
+    id: 50,
+    person: "Laila",
+  },
+];
